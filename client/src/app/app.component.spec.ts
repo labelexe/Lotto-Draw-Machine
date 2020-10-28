0d55441468fa4429dd0d05bc13e3cdaa7018cd0e
don Pablo <don@obeyi.com>
2020-10-28 12:16:28 +0200

    Integration Prep

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'lotto-web-client-angular'`, async(() => {
+  it(`should have as title 'lotto-draw-machine-client'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('lotto-web-client-angular');
+    expect(app.title).toEqual('lotto-draw-machine-client');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to lotto-web-client-angular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to lotto-draw-machine-client!');
   }));
 });
